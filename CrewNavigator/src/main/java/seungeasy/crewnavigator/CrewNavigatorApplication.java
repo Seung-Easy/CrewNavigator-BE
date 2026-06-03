58b6a6e47282eadb5bfd8b2192d299f0f9b8cc93
sglee <sglee.dev@gmail.com>
2026-06-03 23:52:10 +0900

    [Feat] Initialize project with core dependencies

---
+++ CrewNavigator/src/main/java/seungeasy/crewnavigator/CrewNavigatorApplication.java
@@ -0,0 +1,13 @@
+package seungeasy.crewnavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewNavigatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrewNavigatorApplication.class, args);
+    }
+
+}
